68eca9054de28a7f7cca3d10c1147e1fd01bc409
mmhuang <mmhuang625@gmail.com>
2024-09-30 16:57:24 +0800

    debug

--- samples/pairing/c_pre.c
+++ samples/pairing/c_pre.c
@@ -511,7 +511,7 @@ char* EMSCRIPTEN_KEEPALIVE keyGenTest(char *inArray, int inLen, char *outArray, 
     (*outLen) = strlen(my_string);
     memcpy(outArray, my_string, (*outLen));
     printf("outLen=%d\n", (*outLen));
-    (*outLen) = (int)'8';
+    (*outLen) = (int)'987';
     return outArray;
 }
 
