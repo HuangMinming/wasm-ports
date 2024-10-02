a554e4294719d13cd2d46c83e100e7db6e564996
mmhuang <mmhuang625@gmail.com>
2024-10-02 19:22:03 +0800

    debug

--- samples/pairing/c_pre.c
+++ samples/pairing/c_pre.c
@@ -74,7 +74,7 @@ uint32_t HexStrToByteStr(const uint8_t * src_buf, int src_len, uint8_t * dest_bu
     for(int i=0;i<src_len/2;i++){
         printf("%02x ", dest_buf[i]);
     }
-    printf("ByteStrToHexStr end:\n");
+    printf("\nByteStrToHexStr end:\n");
     return 0;
 }
 
@@ -782,7 +782,7 @@ int Enc2(unsigned char *pk_Hex_data, int pk_Hex_data_len)
     int iret = HexStrToByteStr((uint8_t *)pk_Hex_data, pk_Hex_data_len, (uint8_t *)pk_data);
     printf("pk_data=\n");
     for(int i=0;i<G1_ELEMENT_LENGTH_IN_BYTES;i++) {
-        printf("%02x ", (uint8_t)pk_data[i]);
+        printf("%02x ", pk_data[i]);
     }
     printf("\n");
 
